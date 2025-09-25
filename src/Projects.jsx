716a8ab9cdb1644a23c224a255e733861e218b93
Colby Ricks <rickscolby@gmail.com>
2025-09-25 01:34:21 -0700

    Project initial styling

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -6,8 +6,56 @@
  */
 
 import Heading from "./components/Heading";
+import Project from "./components/Project";
 
 function Projects({imgUrl}) {
+  let projects = [
+    {
+      title: "Project title 1",
+      description: "2677 sq. feet",
+      measure: "2677 sq. ft",
+      address: "XXXXX Address Way, San Diego, CA",
+      images: [
+        "https://thumbs.dreamstime.com/b/many-tall-buildings-under-construction-cranes-20004270.jpg",
+        "https://media.istockphoto.com/id/450837501/photo/new-industry-construction.jpg?s=612x612&w=0&k=20&c=OB2tQl6xfHQwrJPlLEwRhfUKIdOiqV7oKJ-tCGYj-TY=",
+        "https://media.gettyimages.com/id/170616024/photo/concrete-highrise-construction-site.jpg?s=612x612&w=gi&k=20&c=UoL4CqK6T_X8j8ybEKnnUeubJ8OG2GUofEciDRkojsc="
+      ]
+    },
+    {
+      title: "Project title 1",
+      description: "2677 sq. feet",
+      measure: "2677 sq. ft",
+      address: "XXXXX Address Way, San Diego, CA",
+      images: [
+        "https://thumbs.dreamstime.com/b/many-tall-buildings-under-construction-cranes-20004270.jpg",
+        "https://media.istockphoto.com/id/450837501/photo/new-industry-construction.jpg?s=612x612&w=0&k=20&c=OB2tQl6xfHQwrJPlLEwRhfUKIdOiqV7oKJ-tCGYj-TY=",
+        "https://media.gettyimages.com/id/170616024/photo/concrete-highrise-construction-site.jpg?s=612x612&w=gi&k=20&c=UoL4CqK6T_X8j8ybEKnnUeubJ8OG2GUofEciDRkojsc="
+      ]
+    },
+    {
+      title: "Project title 1",
+      description: "2677 sq. feet",
+      measure: "2677 sq. ft",
+      address: "XXXXX Address Way, San Diego, CA",
+      images: [
+        "https://thumbs.dreamstime.com/b/many-tall-buildings-under-construction-cranes-20004270.jpg",
+        "https://media.istockphoto.com/id/450837501/photo/new-industry-construction.jpg?s=612x612&w=0&k=20&c=OB2tQl6xfHQwrJPlLEwRhfUKIdOiqV7oKJ-tCGYj-TY=",
+        "https://media.gettyimages.com/id/170616024/photo/concrete-highrise-construction-site.jpg?s=612x612&w=gi&k=20&c=UoL4CqK6T_X8j8ybEKnnUeubJ8OG2GUofEciDRkojsc="
+      ]
+    },
+    {
+      title: "Project title 1",
+      description: "2677 sq. feet",
+      measure: "2677 sq. ft",
+      address: "XXXXX Address Way, San Diego, CA",
+      images: [
+        "https://thumbs.dreamstime.com/b/many-tall-buildings-under-construction-cranes-20004270.jpg",
+        "https://media.istockphoto.com/id/450837501/photo/new-industry-construction.jpg?s=612x612&w=0&k=20&c=OB2tQl6xfHQwrJPlLEwRhfUKIdOiqV7oKJ-tCGYj-TY=",
+        "https://media.gettyimages.com/id/170616024/photo/concrete-highrise-construction-site.jpg?s=612x612&w=gi&k=20&c=UoL4CqK6T_X8j8ybEKnnUeubJ8OG2GUofEciDRkojsc="
+      ]
+    }
+  ]
+
   return (
     <main id="projects">
         <img className="background" src={imgUrl}></img>
@@ -15,7 +63,9 @@ function Projects({imgUrl}) {
         <Heading title={"Projects"} />
 
         <section>
-            {/* List of projects in this section */}
+          {projects.map((project, index) => {
+            return <Project key={index} project={project} />
+          })}
         </section>
     </main>
   );
--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,9 +1,16 @@
-/* Future plans to add image gallery with pics from site? */
-
-function Project({title, imgUrl}) {
+function Project({project}) {
   return (
-    <article>
-
+    <article className="project">
+      <div className="project-carousel">
+        {/* Image carousel logic */}
+        <img src={project.images[0]} alt="" />
+      </div>
+      <div className="project-info">
+        <h1>{project.title}</h1>
+        <p><i class="fa-regular fa-map"></i>{project.address}</p>
+        <p><i class="fa-regular fa-calendar"></i>{project.description}</p>
+        <p><i class="fa-regular fa-ruler"></i>{project.measure}</p>
+      </div>
     </article>
   );
 }
