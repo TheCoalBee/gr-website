6204ff48a119b19595899cdb208d6830f63ec62e
Colby Ricks <rickscolby@gmail.com>
2025-09-26 17:50:03 -0700

    Adjust call to action and hero buttons and text

--- src/components/CallToAction.jsx
+++ src/components/CallToAction.jsx
@@ -8,8 +8,7 @@ function CallToAction() {
             <h1>Book us for your next project.</h1>
             <p>If you have a project in mind, get in touch with our team of experts to turn your dream into a reality. We can assist with budgetary, cost, and design options. We are not just the mason- we become a partner with your team.</p>
             <div>
-                <button>Get a Free Quote</button>
-                <button>Contact Us</button>
+                <button>Get a free quote</button>
             </div>
         </section>
     );
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,6 @@ function Hero() {
   return (
     <section id="hero">
         <h1>Building Modern Spaces with Precision & Care</h1>
-        <p>From design to construction, we deliver quality results for homes and businesses.</p>
         <Link to="/contact-us"><button>Get a Free Quote</button></Link>
     </section>
   );
