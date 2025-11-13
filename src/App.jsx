f61d67893f2ba372810071cb1c68167db3923ea8
Colby Ricks <rickscolby@gmail.com>
2025-11-13 10:54:20 -0800

    fix basenames

--- src/App.jsx
+++ src/App.jsx
@@ -10,20 +10,19 @@ import NotFound from "./NotFound";
 
 export default function App() {
   return (
-    <BrowserRouter basename="/">
-        <ScrollToTop />
+    <BrowserRouter basename="/gr-website">
+      <ScrollToTop />
+      <Header />
 
-        <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="projects" element={<Projects imgUrl="https://media.istockphoto.com/id/479842074/photo/empty-road-at-building-exterior.jpg?s=612x612&w=0&k=20&c=SbyfZGN0i2O_QPLCdBcu9vhuzbQvTz4bGEn-lIzrN0E=" />} />
+        <Route path="contact-us" element={<Contact imgUrl="https://www.shutterstock.com/image-photo/stunning-view-new-york-city-600nw-2500167629.jpg" />} />
+        <Route path="about-us" element={<About imgUrl="https://images.pexels.com/photos/302769/pexels-photo-302769.jpeg?cs=srgb&dl=pexels-pixabay-302769.jpg&fm=jpg" />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
 
-        <Routes>
-            <Route path="/gr-website" element={<Home />} />
-            <Route path="/gr-website/projects" element={<Projects imgUrl={"https://media.istockphoto.com/id/479842074/photo/empty-road-at-building-exterior.jpg?s=612x612&w=0&k=20&c=SbyfZGN0i2O_QPLCdBcu9vhuzbQvTz4bGEn-lIzrN0E="}/>} />
-            <Route path="/gr-website/contact-us" element={<Contact imgUrl={"https://www.shutterstock.com/image-photo/stunning-view-new-york-city-600nw-2500167629.jpg"}/>} />
-            <Route path="/gr-website/about-us" element={<About imgUrl={"https://images.pexels.com/photos/302769/pexels-photo-302769.jpeg?cs=srgb&dl=pexels-pixabay-302769.jpg&fm=jpg"}/>} />
-            <Route path="*" element={<NotFound />} />
-        </Routes>
-
-        <Footer />
+      <Footer />
     </BrowserRouter>
   );
 }
