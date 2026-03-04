fd139fbf35a7cf352901f67c9777374daf2a9dfc
Colby Ricks <rickscolby@gmail.com>
2026-03-04 08:15:15 -0800

    Website in its current state

--- src/App.jsx
+++ src/App.jsx
@@ -12,11 +12,50 @@ import NotFound from "./NotFound";
 
 export default function App() {
   useEffect(() => {
+    // Comprehensive image preloading to prevent grey screen while loading
     const imagesToPreload = [
+      // Logo
+      "G&R Constructors, Inc_Option-01.png",
+      
+      // Home page
+      "home-background.png",
+      "home-block-1.png",
+      "home-block-2.png",
+      "home-block-3.png",
+      
+      // Projects page
+      "projects-background.png",
+      "Archibald Library.png",
+      "Granger JHS Building.png",
+      
+      // About page
+      "about-background.png",
+      "about-us-block-1.png",
+      "about-us-block-2.png",
+      
+      // Services page
+      "services-background.png",
+      "services-images/acoustical-ceilings.png",
+      "services-images/acoustical-tackable-wall-panels.png",
+      "services-images/framing-wood-metal.png",
+      "services-images/taping-drywall.png",
+      "services-images/insulation.png",
+      "services-images/exterior-cladding-systems-1.png",
+      
+      // Team member photos
+      "EBP5769Antz.jpg",
+      "EBP5645Antz.jpg",
+      "EBP5705Antz.jpg",
+      "winegardner-5495.png",
+      "winegardner-5593.jpg",
+      
+      // Auto-images (optimized versions)
+      "/auto-images/home-bg.webp",
       "/auto-images/projects-bg.jpg",
-      "/services-background.jpg",
       "/auto-images/contact-bg.jpg",
       "/auto-images/about-bg.jpg",
+      "/auto-images/notfound-bg.jpg",
+      "Favicon-01.ico",
     ];
 
     imagesToPreload.forEach((src) => {
