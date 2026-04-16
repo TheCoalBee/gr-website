b3892a58b67a78b7abab18cfaf5dc1a296da95e5
Colby Ricks <rickscolby@gmail.com>
2026-04-16 13:19:49 -0700

    Remove team members and don't display roles

--- src/About.jsx
+++ src/About.jsx
@@ -10,32 +10,26 @@ import Heading from "./components/misc/Heading";
 import TeamMembers from "./components/about/TeamMembers";
 import CallToAction from "./components/misc/CallToAction";
 import ContentBlock from "./components/home/ContentBlock";
-import ContentText from "./components/home/ContentText"
+import ContentText from "./components/home/ContentText";
 
-function About({imgUrl}) {
+function About({ imgUrl }) {
   let departments = [
     {
       name: "Accounting",
       teamMembers: [
         {
-          name: "Alex Ayala",
-          role: "Controller",
-          picture: "EBP5769Antz.webp",
-          offset: "0 10%"
-        },
-        {
           name: "Jackie Duran",
           role: "Accounting",
           picture: "winegardner-5593.webp",
-          offset: "0 12%"
+          offset: "0 12%",
         },
         {
           name: "Frankie Henderson",
           role: "Labor Compliance and Payroll",
           picture: "EBP5645Antz.webp",
-          offset: "0 23%"
-        }
-      ]
+          offset: "0 23%",
+        },
+      ],
     },
     {
       name: "Operations",
@@ -47,46 +41,73 @@ function About({imgUrl}) {
           offset: "0% 30%",
         },
         {
-          name: "Armando Ramirez",
-          role: "Project Estimator",
-          picture: "EBP5705Antz.webp",
-          offset: "0% 10%",
-        },
-        {
           name: "Chris Contla",
           role: "Project Manager",
-          picture: "https://t4.ftcdn.net/jpg/00/68/63/23/360_F_68632352_kmHLwFc2rQLmnKqn6gM0bhOPqxRTx8sY.jpg",
+          picture:
+            "https://t4.ftcdn.net/jpg/00/68/63/23/360_F_68632352_kmHLwFc2rQLmnKqn6gM0bhOPqxRTx8sY.jpg",
           offset: "0% 25%",
         },
-      ]
+      ],
     },
-  ]
+    {
+      name: "Pre-construction",
+      teamMembers: [
+        {
+          name: "Jose Garcia",
+          role: "Preconstruction Manager",
+          picture:
+            "https://t4.ftcdn.net/jpg/00/68/63/23/360_F_68632352_kmHLwFc2rQLmnKqn6gM0bhOPqxRTx8sY.jpg",
+          offset: "0% 25%",
+        },
+      ],
+    },
+  ];
   return (
     <main id="about-us">
-        <img loading="lazy" className="background" src={imgUrl}></img>
+      <img loading="lazy" className="background" src={imgUrl}></img>
 
-        <Heading title={"About Us"} content={"Here at G&R Constructors, we build more than structures — we build trust, integrity, and lasting relationships. Meet the team driving excellence from the ground up."}/>
+      <Heading
+        title={"About Us"}
+        content={
+          "Here at G&R Constructors, we build more than structures — we build trust, integrity, and lasting relationships. Meet the team driving excellence from the ground up."
+        }
+      />
 
-       <ContentBlock 
-            alignment="left" 
-            wordContent={<ContentText header="The Team That Builds Your Vision" paragraph="G&R Constructors was founded on a simple idea: great work comes from great people. Behind every successful project is a team that makes it happen. Our skilled professionals combine innovation, precision, and teamwork to deliver results you can rely on every time."/>}
-            imageContent={<img loading="lazy" src="about-us-block-1.webp"/>}
-            theme="light"
+      <ContentBlock
+        alignment="left"
+        wordContent={
+          <ContentText
+            header="The Team That Builds Your Vision"
+            paragraph="G&R Constructors was founded on a simple idea: great work comes from great people. Behind every successful project is a team that makes it happen. Our skilled professionals combine innovation, precision, and teamwork to deliver results you can rely on every time."
           />
-          <ContentBlock 
-            alignment="right" 
-            wordContent={<ContentText header="Built on a Strong Foundation" paragraph="G&R Constructors was created by a team of lifelong builders who share a commitment to doing things the right way. With a focus on precision, reliability, and craftsmanship, the company has grown from a small partnership into a trusted provider of residential, commercial, and custom construction services. Today, G&R continues to build with integrity — delivering projects that stand the test of time."/>}
-            imageContent={<img loading="lazy" src="about-us-block-2.webp"/>}
-            theme="dark"
+        }
+        imageContent={<img loading="lazy" src="about-us-block-1.webp" />}
+        theme="light"
+      />
+      <ContentBlock
+        alignment="right"
+        wordContent={
+          <ContentText
+            header="Built on a Strong Foundation"
+            paragraph="G&R Constructors was created by a team of lifelong builders who share a commitment to doing things the right way. With a focus on precision, reliability, and craftsmanship, the company has grown from a small partnership into a trusted provider of residential, commercial, and custom construction services. Today, G&R continues to build with integrity — delivering projects that stand the test of time."
           />
+        }
+        imageContent={<img loading="lazy" src="about-us-block-2.webp" />}
+        theme="dark"
+      />
 
-        <section id="departments">
-          {departments.map((department, index) => {
-            return <div key={index}><h1>{department.name}</h1><TeamMembers department={department}/></div>
-          })}
-        </section>
+      <section id="departments">
+        {departments.map((department, index) => {
+          return (
+            <div key={index}>
+              <h1>{department.name}</h1>
+              <TeamMembers department={department} />
+            </div>
+          );
+        })}
+      </section>
 
-        <CallToAction />
+      <CallToAction />
     </main>
   );
 }
--- src/components/about/TeamMember.jsx
+++ src/components/about/TeamMember.jsx
@@ -1,18 +1,20 @@
-function TeamMember({teamMember}) {
+function TeamMember({ teamMember }) {
   return (
     <article className="team-member">
-        <div className="team-member-picture">
-          <img loading="lazy"
+      <div className="team-member-picture">
+        <img
+          loading="lazy"
           src={teamMember.picture}
           style={{
-            objectPosition: teamMember.offset}}
-          alt=""/>
-        </div>
-        
-        <div className="team-member-info">
-            <h1>{teamMember.name}</h1>
-            <p>{teamMember.role}</p>
-        </div>
+            objectPosition: teamMember.offset,
+          }}
+          alt=""
+        />
+      </div>
+
+      <div className="team-member-info">
+        <h1>{teamMember.name}</h1>
+      </div>
     </article>
   );
 }
